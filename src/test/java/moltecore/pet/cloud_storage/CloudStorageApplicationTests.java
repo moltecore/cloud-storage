0abc0e2cb9baa8ea46e6b435a1ad4d9a102ddd00
moltecore <proektdrozdov@yandex.ry>
2026-06-30 13:40:34 +0300

    Initial Spring Boot project

---
+++ src/test/java/moltecore/pet/cloud_storage/CloudStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package moltecore.pet.cloud_storage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
